059469ad300e88116d5ebf82ec80066981714183
ViktoriTanos <99451694+Viktoriaset@users.noreply.github.com>
2023-11-20 08:52:54 +1000

    Добавьте файлы проекта.

---
+++ Back/GameField.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeInRow.Back
+{
+    public class GameField
+    {
+        private int[,] _field;
+
+        public GameField(int rowsCount, int columnsCount)
+        {
+            _field = new int[rowsCount, columnsCount];
+        }
+
+        public void FillRandomElements()
+        {
+
+        }
+
+
+    }
+}
---
+++ MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreeInRow
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var frm = new Level1();
+            frm.Location = this.Location;
+            frm.StartPosition = FormStartPosition.Manual;
+            frm.FormClosing += delegate { this.Show(); };
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
